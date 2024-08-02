f54b01e2427273b638bdca3e66635f7d22a5a010
kobayashi1987 <sashagao1987@gmail.com>
2024-08-02 22:11:27 +0800

    run the first deployment using script

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
-        vm.startBroadcast();
+        vm.startBroadcast(); // anything we want to sent need to be put in the broadcast(start -> stop)
 
         SimpleStorage simpleStorage = new SimpleStorage();
 
