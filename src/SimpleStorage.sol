efbd0b354b25054abc05a50cb5ebe37041306bc5
kobayashi1987 <sashagao1987@gmail.com>
2024-08-02 22:22:41 +0800

    little change

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -13,7 +13,7 @@ contract SimpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
+    
     People[] public people;
 
     mapping(string => uint256) public nameToFavoriteNumber;
