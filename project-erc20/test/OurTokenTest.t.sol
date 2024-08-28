c4eca8de8c50b007ca2853959f210623e4f4d388
kobayashi1987 <sashagao1987@gmail.com>
2024-08-28 16:09:41 +0800

    2024 aug 28 erc-20

---
+++ project-erc20/test/OurTokenTest.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+// import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+
+contract OurTokenTest is Test {
+    uint256 public constant BOB_STARTING_AMOUNT = 100 ether;
+
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+    address public deployerAddress;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, BOB_STARTING_AMOUNT);
+    }
+
+    function testBobBalance() public {
+        assertEq(BOB_STARTING_AMOUNT, ourToken.balanceOf(bob));
+    }
+
+    function testAllowances() public {
+        uint256 initialAllowance = 1000;
+
+        // Bob approves Alice to spend tokens on his behalf
+
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), BOB_STARTING_AMOUNT - transferAmount);
+    }
+}
