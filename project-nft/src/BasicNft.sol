e70ae0c3303ecd3ddae80a96464afabe5b42c7f7
kobayashi1987 <sashagao1987@gmail.com>
2024-09-06 21:18:11 +0800

    2024 sep 06 NFT

---
+++ project-nft/src/BasicNft.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC721/ERC721.sol)
+
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "Dog") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {}
+}
