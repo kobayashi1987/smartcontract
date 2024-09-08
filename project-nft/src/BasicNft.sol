d0e87b57e41beb38a9284863f295eb5701f500bb
kobayashi1987 <sashagao1987@gmail.com>
2024-09-08 21:00:42 +0800

    2024 sep 08

--- project-nft/src/BasicNft.sol
+++ project-nft/src/BasicNft.sol
@@ -6,15 +6,22 @@ pragma solidity ^0.8.20;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
+    mapping(uint256 tokenId => string tokenUri) private s_tokenIdToUri;
     uint256 private s_tokenCounter;
 
-    constructor() ERC721("Dogie", "Dog") {
+    constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+    }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
+    }
 }
---
+++ project-nft/test/BasicNftTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testBasicNft() public {
+        DeployBasicNft deployBasicNft = new DeployBasicNft();
+    }
+
+    function testNameIsCorrect() public {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+}
