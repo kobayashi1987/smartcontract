cafab22f2cc670d142c67343488f22421213abe0
kobayashi1987 <sashagao1987@gmail.com>
2024-09-24 21:22:00 +0800

    2024 Sep 24 Defi

--- defi-stablecoin/test/fuzz/Handler.t.sol
+++ defi-stablecoin/test/fuzz/Handler.t.sol
@@ -1,3 +1,21 @@
-// SPDX-License-Identifier: MIT
+// Commented out for now until revert on fail == false per function customization is implemented
+
+// // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
+
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import { Test, console2 } from "forge-std/Test.sol";
+// import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol"; Updated mock location
+import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+
+import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
+import { DSCEngine, AggregatorV3Interface } from "../../src/DSCEngine.sol";
+import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
+// import {Randomish, EnumerableSet} from "../Randomish.sol"; // Randomish is not found in the codebase, EnumerableSet
+// is imported from openzeppelin
+import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
+import { console } from "forge-std/console.sol";
+
+
+contract Handler is Test {}
---
+++ defi-stablecoin/test/fuzz/OpenInvariantsTest.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+// // Invariants:
+// // protocol must never be insolvent / undercollateralized
+// // TODO: users cant create stablecoins with a bad health factor
+// // TODO: a user should only be able to be liquidated if they have a bad health factor
+
+// import { Test, console2 } from "forge-std/Test.sol";
+// import { StdInvariant } from "forge-std/StdInvariant.sol";
+// import { DSCEngine } from "../../src/DSCEngine.sol";
+// import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
+// import { HelperConfig } from "../../script/HelperConfig.s.sol";
+// import { DeployDSC } from "../../script/DeployDSC.s.sol";
+// // import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol"; Updated mock location
+// // import { ERC20Mock } from "../../mocks/ERC20Mock.sol";
+// // import { StopOnRevertHandler } from "./StopOnRevertHandler.t.sol";
+// import { console } from "forge-std/console.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// contract OpenInvariantsTest is StdInvariant, Test {
+//     DeployDSC deployer;
+//     DSCEngine dsce;
+//     DecentralizedStableCoin dsc;
+//     HelperConfig config;
+
+
+//     address public ethUsdPriceFeed;
+//     address public btcUsdPriceFeed;
+//     address public weth;
+//     address public wbtc;
+
+//     uint256 amountCollateral = 10 ether;
+//     uint256 amountToMint = 100 ether;
+
+//     uint256 public constant STARTING_USER_BALANCE = 10 ether;
+//     address public constant USER = address(1);
+//     uint256 public constant MIN_HEALTH_FACTOR = 1e18;
+//     uint256 public constant LIQUIDATION_THRESHOLD = 50;
+
+//     // Liquidation
+//     address public liquidator = makeAddr("liquidator");
+//     uint256 public collateralToCover = 20 ether;
+
+//     function setUp() external {
+//         deployer = new DeployDSC();
+//         (dsc, dsce, config) = deployer.run();
+//         (,, weth, wbtc,) = config.activeNetworkConfig();
+//         targetContract(address(dsce));
+//         // targetContract(address(ethUsdPriceFeed));// Why can't we just do this?
+//     }
+
+//     function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars() public view {
+//         // get the value of all the collateral in protocal 
+//         // compare it to the total supply of DSC
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 totalWelthDeposited = IERC20(weth).balanceOf(address(dsce));
+//         uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+//         uint256 wethValue = dsce.getUsdValue(weth, totalWelthDeposited);
+//         uint256 wbtcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+//         console2.log("wethValue:", wethValue);
+//         console2.log("wbtcValue:", wbtcValue);
+//         console2.log("totalSupply:", totalSupply);
+
+//         assert(wethValue + wbtcValue >= totalSupply);
+//     }
+
+   
+// }
