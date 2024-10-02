66c9aacdce9b1c449aab4afcced794610db144f4
kobayashi1987 <sashagao1987@gmail.com>
2024-10-02 21:50:20 +0800

    Merkle Airdop: 2024 oct 02

---
+++ merkle-airdrop/src/MerkleAirdrop.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+contract MerkleAirDrop {
+    // list of addresses
+    // Allow someone in the list to claim tokens
+    address[] public claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claimer(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+    }
+}
